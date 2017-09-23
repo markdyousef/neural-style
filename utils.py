0c228b7a1ed8a984eedb6e1f8f6b382461d14322
markdyousef <markdyousef@gmail.com>
2017-09-23 16:34:36 +0200

    created architecture blocks and Style Transfer Network

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ from keras.models import Model
 import keras.backend as K
 from keras.layers import *
 from keras import metrics
+from keras.preprocessing import image
 from glob import glob
 from PIL import Image
 import numpy as np
@@ -18,3 +19,10 @@ def save_array(fname, arr):
     c.flush()
 
 def load_array(fname): return bz.open(fname)[:]
+
+def get_batches(path, gen=image.ImageDataGenerator(), shuffle=True, batch_size=64, class_mode='categorical', target_size=(256,256)):
+    return gen.flow_from_directory(path, target_size=target_size, batch_size=batch_size, class_mode=class_mode, shuffle=shuffle)
+
+def get_data(path, target_size=(256,256)):
+    batches = get_batches(path, target_size=target_size)
+    return np.concatenate([batches.next() for i in range (batches.samples)])
